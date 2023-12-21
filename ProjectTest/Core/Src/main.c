4e70f1a9eda96234f1e8087e2de3581d936004b3
Przemysław Brzęk <przemyslaw.brzek@hotmail.com>
2023-12-21 16:37:28 +0100

    Added LED Segment Display

--- ProjectTest/Core/Src/main.c
+++ ProjectTest/Core/Src/main.c
@@ -31,6 +31,7 @@ typedef uint32_t TIM_Channel;
 
 UART_HandleTypeDef huart3;
 TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim9;
 
 typedef struct HCSR04 {
@@ -48,7 +49,7 @@ HCSR04 distanceSensor = {
 };
 
 void HCSR04_Init(HCSR04* hcsr04) {
-	HAL_TIM_IC_Start_IT(hcsr04->echoTimer, hcsr04->echoChannel);
+	//HAL_TIM_IC_Start_IT(hcsr04->echoTimer, hcsr04->echoChannel);
 	HAL_TIM_PWM_Start(hcsr04->trigTimer, hcsr04->trigChannel);
 }
 
@@ -56,38 +57,119 @@ uint32_t HCSR04_Read(HCSR04* hcsr04) {
 	return HAL_TIM_ReadCapturedValue(hcsr04->echoTimer, hcsr04->echoChannel) / 58;
 }
 
-uint32_t val[10];
+typedef struct SegmentDisplay {
+	GPIO_TypeDef* selectPorts[3];
+	uint16_t selectPins[3];
+	GPIO_TypeDef* digitPorts[8];
+	uint16_t digitPins[8];
+} SegmentDisplay;
+
+// 11, 7, 4, 2, 1, 10, 3
+uint8_t DIGIT_SEGMENTS[] = {
+	0b11111100, // 0
+	0b01100000, // 1
+	0b11011010, // 2
+	0b11110010, // 3
+	0b01100110, // 4
+	0b10110110, // 5
+	0b10111110, // 6
+	0b11100000, // 7
+	0b11111110, // 8
+	0b11110110, // 9
+};
+
+SegmentDisplay display = {
+	.selectPorts = { SD_8_GPIO_Port, SD_9_GPIO_Port, SD_12_GPIO_Port },
+	.selectPins = { SD_8_Pin, SD_9_Pin, SD_12_Pin },
+	.digitPorts = { SD_11_GPIO_Port, SD_7_GPIO_Port, SD_4_GPIO_Port, SD_2_GPIO_Port, SD_1_GPIO_Port, SD_10_GPIO_Port, SD_5_GPIO_Port, SD_3_GPIO_Port },
+	.digitPins = { SD_11_Pin, SD_7_Pin, SD_4_Pin, SD_2_Pin, SD_1_Pin, SD_10_Pin, SD_5_Pin, SD_3_Pin }
+};
+
+void SegmentDisplay_Write(SegmentDisplay* sd, uint32_t val) {
+	int tmp = val;
+	for (int i = 0; i < 3; ++i) {
+		int digit = DIGIT_SEGMENTS[tmp % 10];
+		if (i == 0) {
+			HAL_GPIO_WritePin(sd->selectPorts[2], sd->selectPins[2], RESET);
+		} else {
+			HAL_GPIO_WritePin(sd->selectPorts[i - 1], sd->selectPins[i - 1], RESET);
+			if (i == 2) {
+				digit |= 1;
+			}
+		}
+
+		for (int j = 0; j < 8; ++j) {
+			if ((digit >> (7 - j)) & 1) {
+				HAL_GPIO_WritePin(sd->digitPorts[j], sd->digitPins[j], RESET);
+			} else {
+				HAL_GPIO_WritePin(sd->digitPorts[j], sd->digitPins[j], SET);
+			}
+		}
+
+		HAL_GPIO_WritePin(sd->selectPorts[i], sd->selectPins[i], SET);
+		HAL_Delay(1);
+		tmp /= 10;
+	}
+}
+
+uint32_t val = 200;
+uint32_t valBuf[10] = { 200, 200, 200, 200, 200, 200, 200, 200, 200, 200 };
 size_t valIdx = 0;
 char buf[256];
+int buzzerDelay1 = 0;
+int buzzerDelay2 = 0;
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
 	if (htim == distanceSensor.echoTimer) {
-		val[valIdx++] = HCSR04_Read(&distanceSensor);
-		if (valIdx >= 10) {
-			uint32_t avg = 0;
-			for (int i = 0; i < 10; ++i) {
-				avg += val[i];
+		valBuf[valIdx++] = HCSR04_Read(&distanceSensor);
+		valIdx %= 10;
+		//if (valIdx >= 10) {
+			uint32_t avg = valBuf[0], vmin = valBuf[0], vmax = valBuf[0];
+			for (int i = 1; i < 10; ++i) {
+				avg += valBuf[i];
+				if (valBuf[i] < vmin) {
+					vmin = valBuf[i];
+				}
+				if (valBuf[i] > vmax) {
+					vmax = valBuf[i];
+				}
 			}
-			avg /= 10;
+			avg -= vmax + vmin;
+			avg /= 8;
+			val = avg;
 
 			int len = sprintf((char*)buf, "Distance: %li\r\n", avg);
 			HAL_UART_Transmit(&huart3, (uint8_t*)buf, len, 9999);
-			valIdx = 0;
+			//valIdx = 0;
 
 			if (avg < 20) {
 				HAL_GPIO_WritePin(LED_Red_GPIO_Port, LED_Red_Pin, SET);
 				HAL_GPIO_WritePin(LED_Yellow_GPIO_Port, LED_Yellow_Pin, RESET);
 				HAL_GPIO_WritePin(LED_Green_GPIO_Port, LED_Green_Pin, RESET);
+				htim2.Instance->CCR4 = 200;
+				buzzerDelay1 = 50;
+				buzzerDelay2 = 0;
 			} else if (avg < 60) {
 				HAL_GPIO_WritePin(LED_Red_GPIO_Port, LED_Red_Pin, RESET);
 				HAL_GPIO_WritePin(LED_Yellow_GPIO_Port, LED_Yellow_Pin, SET);
 				HAL_GPIO_WritePin(LED_Green_GPIO_Port, LED_Green_Pin, RESET);
+				htim2.Instance->CCR4 = 100;
+				buzzerDelay1 = 3 * avg;
+				buzzerDelay2 = 3 * avg;
 			} else {
 				HAL_GPIO_WritePin(LED_Red_GPIO_Port, LED_Red_Pin, RESET);
 				HAL_GPIO_WritePin(LED_Yellow_GPIO_Port, LED_Yellow_Pin, RESET);
 				HAL_GPIO_WritePin(LED_Green_GPIO_Port, LED_Green_Pin, SET);
+				if (avg < 175) {
+					htim2.Instance->CCR4 = 60 + (20*(175 - avg))/(175 - 60);
+				} else {
+					htim2.Instance->CCR4 = 0;
+				}
+				buzzerDelay1 = 3 * avg;
+				buzzerDelay2 = 3 * avg;
 			}
-		}
+
+		//}
 	}
 }
 /* USER CODE END PTD */
@@ -105,6 +187,7 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
 /* Private variables ---------------------------------------------------------*/
 
 TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim9;
 
 UART_HandleTypeDef huart3;
@@ -122,6 +205,7 @@ static void MX_USART3_UART_Init(void);
 static void MX_USB_OTG_FS_PCD_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_TIM9_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -163,14 +247,25 @@ int main(void)
   MX_USB_OTG_FS_PCD_Init();
   MX_TIM1_Init();
   MX_TIM9_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
   HCSR04_Init(&distanceSensor);
+  //HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_4);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+
   while (1)
   {
+	  SegmentDisplay_Write(&display, 7);
+	//HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_4);
+	//HAL_Delay(buzzerDelay1);
+	//if (buzzerDelay2 > 0){
+	//	HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_4);
+	//	HAL_Delay(buzzerDelay2);
+	//}
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -301,6 +396,55 @@ static void MX_TIM1_Init(void)
   /* USER CODE BEGIN TIM1_Init 2 */
 
   /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 274;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 512;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+  HAL_TIM_MspPostInit(&htim2);
 
 }
 
@@ -443,17 +587,31 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOG_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, LD1_Pin|LD3_Pin|Test_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, LD1_Pin|SD_5_Pin|SD_7_Pin|LD3_Pin
+                          |SD_10_Pin|SD_1_Pin|SD_2_Pin|Test_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(SD_12_GPIO_Port, SD_12_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOE, LED_Red_Pin|LED_Yellow_Pin|LED_Green_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, SD_8_Pin|SD_9_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(USB_PowerSwitchOn_GPIO_Port, USB_PowerSwitchOn_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, SD_11_Pin|SD_3_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(SD_4_GPIO_Port, SD_4_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : USER_Btn_Pin */
   GPIO_InitStruct.Pin = USER_Btn_Pin;
@@ -469,20 +627,29 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : RMII_REF_CLK_Pin RMII_MDIO_Pin RMII_CRS_DV_Pin */
-  GPIO_InitStruct.Pin = RMII_REF_CLK_Pin|RMII_MDIO_Pin|RMII_CRS_DV_Pin;
+  /*Configure GPIO pins : RMII_REF_CLK_Pin RMII_CRS_DV_Pin */
+  GPIO_InitStruct.Pin = RMII_REF_CLK_Pin|RMII_CRS_DV_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
   GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LD1_Pin LD3_Pin Test_Pin */
-  GPIO_InitStruct.Pin = LD1_Pin|LD3_Pin|Test_Pin;
+  /*Configure GPIO pins : LD1_Pin SD_5_Pin SD_7_Pin LD3_Pin
+                           SD_10_Pin SD_1_Pin SD_2_Pin Test_Pin */
+  GPIO_InitStruct.Pin = LD1_Pin|SD_5_Pin|SD_7_Pin|LD3_Pin
+                          |SD_10_Pin|SD_1_Pin|SD_2_Pin|Test_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : SD_12_Pin */
+  GPIO_InitStruct.Pin = SD_12_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(SD_12_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LED_Red_Pin LED_Yellow_Pin LED_Green_Pin */
   GPIO_InitStruct.Pin = LED_Red_Pin|LED_Yellow_Pin|LED_Green_Pin;
@@ -491,13 +658,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : RMII_TXD1_Pin */
-  GPIO_InitStruct.Pin = RMII_TXD1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  /*Configure GPIO pins : SD_8_Pin SD_9_Pin */
+  GPIO_InitStruct.Pin = SD_8_Pin|SD_9_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
-  HAL_GPIO_Init(RMII_TXD1_GPIO_Port, &GPIO_InitStruct);
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
   /*Configure GPIO pin : USB_PowerSwitchOn_Pin */
   GPIO_InitStruct.Pin = USB_PowerSwitchOn_Pin;
@@ -511,6 +677,20 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(USB_OverCurrent_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SD_11_Pin SD_3_Pin */
+  GPIO_InitStruct.Pin = SD_11_Pin|SD_3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : SD_4_Pin */
+  GPIO_InitStruct.Pin = SD_4_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(SD_4_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : RMII_TX_EN_Pin RMII_TXD0_Pin */
   GPIO_InitStruct.Pin = RMII_TX_EN_Pin|RMII_TXD0_Pin;
